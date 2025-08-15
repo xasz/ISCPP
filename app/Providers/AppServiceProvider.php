9a27de32a0afdd9721001b67cf8603777a571252
Michael Schneider <ms.online@ifmore.de>
2025-08-15 22:47:24 +0200

    deploy fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,18 +29,21 @@ class AppServiceProvider extends ServiceProvider
 
         if (!file_exists($dbPath)) {
             file_put_contents($dbPath, '');
-            
-            DB::table('migrations')->where('migration', "0001_01_01_000002_jobs_to_sqlite")->delete();
-            Artisan::call('migrate', [
-                '--force' => true,
-                '--path' => 'database/migrations/' . "0001_01_01_000002_jobs_to_sqlite" . '.php',
-            ]);
-            
-            DB::table('migrations')->where('migration', '0001_01_01_000001_create_cache_table_to_sqlite')->delete();
-            Artisan::call('migrate', [
-                '--force' => true,
-                '--path' => 'database/migrations/' . "0001_01_01_000001_create_cache_table_to_sqlite" . '.php',
-            ]);
+            try{
+                DB::table('migrations')->where('migration', "0001_01_01_000002_jobs_to_sqlite")->delete();
+                Artisan::call('migrate', [
+                    '--force' => true,
+                    '--path' => 'database/migrations/' . "0001_01_01_000002_jobs_to_sqlite" . '.php',
+                ]);
+                
+                DB::table('migrations')->where('migration', '0001_01_01_000001_create_cache_table_to_sqlite')->delete();
+                Artisan::call('migrate', [
+                    '--force' => true,
+                    '--path' => 'database/migrations/' . "0001_01_01_000001_create_cache_table_to_sqlite" . '.php',
+                ]);
+            } catch (\Exception $e) {
+                Log::warning('We are probably running the deployment for the first time, so we will not run the migrations for the cache and jobs tables.');
+            }
         }
     }
 }
