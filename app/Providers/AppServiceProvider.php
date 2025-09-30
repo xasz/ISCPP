fdf7b6400bcfb5b7f902026e13dd0d98551c77a8
Michael Schneider <schneider@ostalb-it.de>
2025-09-30 12:03:21 +0200

    Support for postgres in docker

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,35 +25,35 @@ class AppServiceProvider extends ServiceProvider
     {
         SCAlert::observe(SCAlertObserver::class);
 
-        $dbPath = database_path('database.sqlite');
 
-        
-        if(config('queue.default') != 'sqlite'){
-            throw new \Exception('Currently only SQLite queues are supported. Please set QUEUE_CONNECTION=sqlite in your .env file.');
+        if(config('queue.default') == 'sqlite'){
+            $dbPath = database_path('database.sqlite');
+
+            if (!file_exists($dbPath)) {
+                file_put_contents($dbPath, '');
+                try{
+                    $connection = config('database.default');
+                    DB::connection($connection)->table('migrations')->where('migration', "0001_01_01_000002_jobs_to_sqlite")->delete();
+                    Artisan::call('migrate', [
+                        '--force' => true,
+                        '--path' => 'database/migrations/' . "0001_01_01_000002_jobs_to_sqlite" . '.php',
+                    ]);
+                    
+                    DB::connection($connection)->table('migrations')->where('migration', '0001_01_01_000001_create_cache_table_to_sqlite')->delete();
+                    Artisan::call('migrate', [
+                        '--force' => true,
+                        '--path' => 'database/migrations/' . "0001_01_01_000001_create_cache_table_to_sqlite" . '.php',
+                    ]);
+                } catch (\Exception $e) {
+                    //Log::warning('We are probably running the deployment for the first time, so we will not run the migrations for the cache and jobs tables.');
+                }
+            }
         }
 
+        
         if(config('cache.default') != 'file'){
             throw new \Exception('Currently only file cache is supported. Please set CACHE_STORE=file in your .env file.');
         }
         
-        if (!file_exists($dbPath)) {
-            file_put_contents($dbPath, '');
-            try{
-                $connection = config('database.default');
-                DB::connection($connection)->table('migrations')->where('migration', "0001_01_01_000002_jobs_to_sqlite")->delete();
-                Artisan::call('migrate', [
-                    '--force' => true,
-                    '--path' => 'database/migrations/' . "0001_01_01_000002_jobs_to_sqlite" . '.php',
-                ]);
-                
-                DB::connection($connection)->table('migrations')->where('migration', '0001_01_01_000001_create_cache_table_to_sqlite')->delete();
-                Artisan::call('migrate', [
-                    '--force' => true,
-                    '--path' => 'database/migrations/' . "0001_01_01_000001_create_cache_table_to_sqlite" . '.php',
-                ]);
-            } catch (\Exception $e) {
-                //Log::warning('We are probably running the deployment for the first time, so we will not run the migrations for the cache and jobs tables.');
-            }
-        }
     }
 }
