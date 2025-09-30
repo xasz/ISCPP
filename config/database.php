f6615fcdf2c91111c130a35444c479a15804521f
Michael Schneider <schneider@ostalb-it.de>
2025-09-30 14:38:04 +0200

    some more fixes

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'database'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
